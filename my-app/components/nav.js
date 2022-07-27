63e63707aff52a2a53c4229c0fb5b336f365bd63
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2022-07-27 16:57:11 +0300

    Add wallet connect btn and logo

---
+++ my-app/components/nav.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Nav.module.css";
+
+export default function Nav() {
+  return (
+    <div className={styles.navContainer}>
+      <h1 className={styles.title}>df</h1>
+    </div>
+  );
+}
