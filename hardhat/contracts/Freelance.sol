6507b00aac36c85ba0b02ab094700fd53efbe273
Jossif Elefteriadis <54796194+jossifelefteriadis@users.noreply.github.com>
2022-08-22 10:42:36 +0300

    Create sc for storing profiles (#2)
    * [WIP] create smart contract for profles
    
    * Refactor
    
    * Refactor readme

---
+++ hardhat/contracts/Freelance.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.16;
+
+contract Freelance {
+    struct Profile {
+        string fName;
+        string lName;
+        string country;
+        string bio;
+    }
+
+    mapping(address => Profile) public profiles;
+    address[] public allAddresses;
+
+    function addProfile(string memory _fName, string memory _lName, string memory _country, string memory _bio) public {
+        Profile memory profile = Profile(_fName, _lName, _country, _bio);
+        profiles[msg.sender] = profile;
+        allAddresses.push(msg.sender);
+    }
+
+    function getAllProfiles() public view returns (Profile[] memory) {
+        Profile[] memory _profiles = new Profile[](allAddresses.length);
+        for(uint8 i = 0; i < allAddresses.length; i++) {
+            address owner = allAddresses[i];
+            _profiles[i] = profiles[owner];
+        }
+        return _profiles;
+    }
+}
